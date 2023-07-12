cc30adb87e2a942e239295a2cc15d55ef5c542de
Javeriya <javeriya.noorisys@gmail.com>
2023-07-12 09:25:18 +0530

    snapchat

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,10 @@ Route::prefix('snapchat')->group(function () {
     Route::get('generate-token', [SnapchatController::class, 'generateToken']);
     Route::get('refresh-token', [SnapchatController::class, 'refreshToken']);
     Route::get('get-all-organizations', [SnapchatController::class, 'getAllOrganizations']);
+    Route::get('get-all-adAccounts', [SnapchatController::class, 'getAllAdAccounts']);
+    Route::get('get-all-campaigns/{id}', [SnapchatController::class, 'getAllCampaigns']);
+    Route::get('create-campaign-form/{id}', [SnapchatController::class, 'createCampaignForm']);
+    Route::post('create-campaign', [SnapchatController::class, 'createCampaign']);
+
 });
 
